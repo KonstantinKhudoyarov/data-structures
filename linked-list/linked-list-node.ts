c4287ec090735e5c269023a99ef00c409e7dd8fa
Konstantin Khudoiarov <kostya040390@gmail.com>
2025-02-04 19:32:02 +0200

    Revert "addFirst method has been added"
    This reverts commit f48310522eefe02b3f6236e96bf4bd28edb921a7.

--- linked-list/linked-list-node.ts
+++
@@ -1,8 +0,0 @@
-export class LinkedListNode<T> {
-    public data: T;
-    public next: LinkedListNode<T> | null;
-    constructor(data: T, next: LinkedListNode<T> | null = null) {
-        this.data = data;
-        this.next = next;
-    }
-}
