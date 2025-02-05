688878f3b1c122de7de965bc4618487f3c902cee
Konstantin Khudoiarov <kostya040390@gmail.com>
2025-02-05 20:17:05 +0200

    method addfirst and tail pointer have been added

--- linked-list/linked-list.ts
+++ linked-list/linked-list.ts
@@ -2,16 +2,41 @@ import {LinkedListNode} from './linked-list-node';
 
 class LinkedList<T> {
     private head: LinkedListNode<T> | null;
+    private tail: LinkedListNode<T> | null;
     private size = 0;
 
     constructor() {
         this.head = null;
+        this.tail = null;
     }
 
     public addFirst(data: T): void {
         const node = new LinkedListNode(data);
+
+        if (this.head === null) {
+            this.head = this.tail = node;
+            this.size++;
+            return;
+        }
+        
         node.next = this.head;
         this.head = node;
         this.size++;
+    }
+
+    public addLast(data: T): void {
+        const node = new LinkedListNode(data);
+
+        if (this.head === null) {
+            this.head = this.tail = node;
+            this.size++;
+            return;
+        }
+
+        if (this.tail !== null) {
+            this.tail.next = node;
+            this.tail = node;
+            this.size++;
+        }
     }
 }
